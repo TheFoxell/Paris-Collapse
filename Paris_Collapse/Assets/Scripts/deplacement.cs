18c6d4ee1b8db2e20270a1a0427649e2a0dfe873
TheFoxell <44344136+TheFoxell@users.noreply.github.com>
2021-04-08 19:53:12 +0200

    Add Project

---
+++ Paris_Collapse/Assets/Scripts/deplacement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class deplacement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3((float) -0.1, 0, 0));
+        }
+        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3((float) 0.1, 0, 0));
+        }
+        
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3( 0, 0, (float) -0.1));
+        }
+        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, 0, (float) 0.1));
+        }
+    }
+}

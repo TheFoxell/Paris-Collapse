b5b9f662dd5870fba07c2aa4f5ed520867bf9d24
Mariesh69 <78351509+Mariesh69@users.noreply.github.com>
2021-04-11 21:27:04 +0200

    voituremouv

---
+++ Paris_Collapse/Assets/Scripts/car.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class car : MonoBehaviour
+{
+    NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = GetDestination();
+
+    }
+    
+    void Update()
+    {
+        if (agent.remainingDistance < 1)
+        {
+            agent.destination = GetDestination();
+        }
+    }
+
+    public Vector3 GetDestination()
+    {
+        int dest = Random.Range(1, 6);
+        return GameObject.Find("Point" + dest).transform.position;
+    }
+}

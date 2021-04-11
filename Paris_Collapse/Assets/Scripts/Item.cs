bdabd185eaaedae0c6499d311d62f371b4d45221
TheFoxell <44344136+TheFoxell@users.noreply.github.com>
2021-04-11 16:29:04 +0200

    Structure Inventaire
    Début de la création des scripts et de la gestion des items par les fichiers .csv

--- Paris_Collapse/Assets/Scripts/Item.cs
+++ Paris_Collapse/Assets/Scripts/Item.cs
@@ -6,4 +6,11 @@ public class Item : ScriptableObject
     new public string name = "New Item";
     public Sprite icon = null;
     public bool isDefaultItem = false;
+    
+    public int deg = 0;
+    public int pen = 0;
+    public int pre = 0;
+    public int cri = 0;
+    public int health = 0;
+    public int price = 0;
 }
---
+++ Paris_Collapse/Assets/Scripts/ReadCSV.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ReadCSV : MonoBehaviour
+{
+
+    public List<Item> itemList; 
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        ReadTextCSV();
+    }
+    
+    //Lit le fichier .csv et créer une liste d'item
+    void ReadTextCSV()
+    {
+        using (StreamReader sr = new StreamReader("Assets/Data/playerInventory.csv"))
+        {
+            string line = sr.ReadLine();
+            while (line != "")
+            {
+                line = sr.ReadLine();
+                Item tmpItem = CreateItem(ParseLine(line));
+                itemList.Add(tmpItem);
+            }
+        }
+    }
+
+    string[] ParseLine(string line)
+    {
+        string[] parseline = line.Split(',');
+        return parseline;
+    }
+
+    Item CreateItem(string[] parseLine)
+    {
+        Item tmpItem = new Item();
+        for (int i = 0; i < parseLine.Length; i++)
+        {
+            //todo
+        }
+        
+        return tmpItem;
+    }
+    
+    
+    
+    
+}

67d720f5089bfa269488d8493bb5124d8daf221f
TheFoxell <44344136+TheFoxell@users.noreply.github.com>
2021-04-12 21:52:50 +0200

    Continue interfaces
    Continue de dev la gestion de l'inventaire

--- Paris_Collapse/Assets/Scripts/InventoryUI.cs
+++ Paris_Collapse/Assets/Scripts/InventoryUI.cs
@@ -5,39 +5,18 @@ using UnityEngine;
 public class InventoryUI : MonoBehaviour
 {
     public Transform itemSlots;
-    Inventory inventory;
 
     InventorySlot[] slots;
 
     // Start is called before the first frame update
     void Start()
     {
-        inventory = Inventory.instance;
-        inventory.onItemChangedCallback += UpdateUI;
-
-        slots = itemSlots.GetComponentsInChildren<InventorySlot>();
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
     
-    void UpdateUI()
-    {
-        for (int i = 0; i < slots.Length; i++)
-        {
-            if (i < inventory.items.Count)
-            {
-                slots[i].AddItem(inventory.items[i]);
-            }
-            else
-            {
-                slots[i].ClearSlot();
-            }
-        }
-        
-        Debug.Log("Update UI");
-    }
 }
--- Paris_Collapse/Assets/Scripts/ReadCSV.cs
+++ Paris_Collapse/Assets/Scripts/ReadCSV.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
@@ -6,47 +7,70 @@ using UnityEngine;
 public class ReadCSV : MonoBehaviour
 {
 
-    public List<Item> itemList; 
-        
+    public List<Item> itemListPlayer;
+    public List<Item> itemList;
+    
+    public string path = "Assets/Data/playerInventory.csv";
+    public Transform ItemSlots;
+    
+    public List<Sprite> sprites;
+    
     // Start is called before the first frame update
     void Start()
     {
         ReadTextCSV();
+        AddItems();
     }
     
     //Lit le fichier .csv et créer une liste d'item
     void ReadTextCSV()
     {
-        using (StreamReader sr = new StreamReader("Assets/Data/playerInventory.csv"))
+        using (StreamReader sr = new StreamReader(path))
         {
-            string line = sr.ReadLine();
-            while (line != "")
+            string line;
+            while (!sr.EndOfStream)
             {
                 line = sr.ReadLine();
-                Item tmpItem = CreateItem(ParseLine(line));
-                itemList.Add(tmpItem);
+                CreateItem(line);
+                
             }
         }
     }
 
-    string[] ParseLine(string line)
+    // Pour chaque ca va créer un Item avec les caractéritiques
+    void CreateItem(string line)
     {
-        string[] parseline = line.Split(',');
-        return parseline;
-    }
-
-    Item CreateItem(string[] parseLine)
-    {
-        Item tmpItem = new Item();
-        for (int i = 0; i < parseLine.Length; i++)
+        
+        foreach (var item in itemList)
         {
-            //todo
+            if (item.name == line)
+            {
+                itemListPlayer.Add(item);
+                Debug.Log("Add "+ item.name );
+            }
         }
         
-        return tmpItem;
     }
-    
-    
-    
+
+    void AddItems()
+    {
+        InventorySlot[] slots;
+
+        slots = ItemSlots.GetComponentsInChildren<InventorySlot>();
+        
+        foreach (var elt in itemListPlayer)
+        {
+            Debug.Log("Count " + itemListPlayer.Count);
+            
+            for (int i = 0; i < slots.Length ; i++)
+            {
+                if (i < itemList.Count)
+                {
+                    slots[i].AddItem(elt);  
+                    Debug.Log("Add UI " + elt);
+                }
+            }
+        }
+    }
     
 }

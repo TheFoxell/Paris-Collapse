9a3319ba21ef3314fc354216aff8f2ebe8da39fa
TheFoxell <44344136+TheFoxell@users.noreply.github.com>
2021-04-15 00:02:10 +0200

    Debug + SaveSystem

---
+++ Paris_Collapse/Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int level = 3;
+    public int health = 75;
+    public int coin = 500;
+
+    public bool saving = true;
+
+    public void SavePlayer()
+    {
+        SaveSystem.SavePlayer(this);
+    }
+
+    public void LoadPlayer()
+    {
+        PlayerData data = SaveSystem.LoadPlayer();
+
+        level = data.level;
+        health = data.health;
+
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+        transform.position = position;
+    }
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            StopSaving();
+            SaveSystem.Delete("player");
+        }
+        
+        
+        
+        if(saving)
+            SavePlayer();
+    }
+
+
+    void StopSaving()
+    {
+        saving = !saving;
+    }
+}

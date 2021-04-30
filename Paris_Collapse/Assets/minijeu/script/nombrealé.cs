bff803f5c81c9553b28d06c40d9fbe56aeb3d154
Mariesh69 <78351509+Mariesh69@users.noreply.github.com>
2021-04-30 22:11:50 +0200

    nombrealéatoire

---
+++ Paris_Collapse/Assets/minijeu/script/nombrealé.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class nombrealé : MonoBehaviour
+{
+    public Text txt;
+    public InputField ifText;
+    [SerializeField] 
+    int nb, nbCoup; 
+    bool endGame = false;
+
+    private void Start()
+    {
+        InitNb();
+    }
+    void InitNb()
+    {
+        ifText.interactable = true;
+        endGame = false;
+        nb = Random.Range(1, 100);
+        nbCoup = 0;
+        txt.text = "Trouve le nombre ?";
+        IfGetFocus();
+    }
+
+    public void Validation()
+    {
+        nbCoup++;
+        int n = int.Parse(ifText.text);
+        ifText.text = null;
+
+        if (n == nb)
+        {
+            txt.text = "Bravo, trouvez en " + nbCoup + " coups\n SPACE pour rejouer";
+            endGame = true;
+            ifText.interactable = false;
+        }
+        else if(n < nb)
+        {
+            txt.text = "Plus Grand, " + nbCoup + " coups";
+        }
+        else if(n > nb)
+        {
+            txt.text = "Plus Petit, " + nbCoup + " coups";
+        }
+        
+        IfGetFocus();
+    }
+
+    void IfGetFocus()
+    {
+        ifText.Select();
+        ifText.ActivateInputField();
+    }
+    void Update()
+    {
+        if (endGame && Input.GetKeyDown(KeyCode.Space))
+        {
+            InitNb();
+        }
+    }
+}

da1f3820c24044c0b6e5fd494fb0258b6c4083e7
TheFoxell <44344136+TheFoxell@users.noreply.github.com>
2021-06-17 00:44:44 +0200

    armory + epifood + menu arcade

--- Paris_Collapse/Assets/minijeu/script/Dé.cs
+++ Paris_Collapse/Assets/minijeu/script/Dé.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Dé : MonoBehaviour
@@ -10,6 +11,8 @@ public class Dé : MonoBehaviour
     private Rigidbody rb;
     bool playdice = false;
     public Text txt;
+    private bool played = false;
+    public Player player;
 
     private void Start()
     {
@@ -36,8 +39,18 @@ public class Dé : MonoBehaviour
         if (playdice && rb.velocity.magnitude == 0)
         {
             playdice = false;
+            played = true;
             StartCoroutine(ShowFace());
         }
+
+        if(played && face == 6)
+        {
+            player.coin += 100;
+            player.exp += 20;
+            SceneManager.LoadScene("ville");
+        }
+        if(played)
+            SceneManager.LoadScene("ville");
     }
 
     IEnumerator ShowFace()
